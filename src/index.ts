13d7994225ae2e1b926eb631dd7a269a1d44b11b
Marek Vigaš <vigas147@gmail.com>
2019-05-04 11:26:01 +0200

    conection to google sheet

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
+import GoogleSpreadsheet from "google-spreadsheet";
+import minimist from "minimist";
+
+import events from "./config/events.json";
+import googleSecret from "./config/google_client_secret.json";
+
 (async () => {
-    console.log("a");
+    const args = minimist(process.argv.slice(2));
+    let event;
+
+    // Select event to process
+    for (const evnt of events) {
+        if (evnt.identifier === args.eventId) {
+            event = evnt;
+        }
+    }
+
+    // Create a document object using the ID of the spreadsheet - obtained from its URL.
+    const doc = new GoogleSpreadsheet(event.spreadsheetId);
+
+    // Authenticate with the Google Spreadsheets API.
+    await doc.useServiceAccountAuth(googleSecret, async (_authErr) => {
+
+      // Get all of the rows from the spreadsheet.
+      doc.getRows(1, async (_err, rows) => {
+        for (const row of rows) {
+            console.log(row);
+            row.postarJano = "test";
+            row.save();
+        }
+      });
+    });
 })();
