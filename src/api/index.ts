6aab7bb9b741603e981a041915d3476dedcf93b7
Marek Vigaš <vigas147@gmail.com>
2019-05-06 02:17:18 +0200

    fix cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,7 +12,7 @@ const PORT = 5000;
 
 app.use((_req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     next();
 });
 
