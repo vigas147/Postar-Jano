53000228b4460fdfc8e71c8922fd4fef565e9e4f
Lukas Macko <xlukas.macko@gmail.com>
2020-06-05 18:18:05 +0200

    Init go api
    Signed-off-by: Lukas Macko <xlukas.macko@gmail.com>

---
+++ src/go-api/cmd/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/MarekVigas/Postar-Jano/internal/api"
+
+	"go.uber.org/zap"
+	"gopkg.in/tomb.v2"
+)
+
+const (
+	hostEnvKey                = "host"
+	portEnvKey                = "port"
+	defaultPort               = "5000"
+	HTTPServerShutdownTimeout = 5 * time.Second
+)
+
+func Run(logger *zap.Logger, fnc func(*tomb.Tomb) error) error {
+
+	// Start catching signals.
+	var t tomb.Tomb
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	// Kill the tomb on signal.
+	t.Go(func() error {
+		select {
+		case sig := <-sigCh:
+			logger.Info("Signal received, terminating...", zap.String("signal", sig.String()))
+
+			// Kill the tomb.
+			t.Kill(nil)
+
+			// Get killed on the next signal.
+			signal.Stop(sigCh)
+
+		case <-t.Dying():
+		}
+		return nil
+	})
+
+	// Call business logic.
+	if err := fnc(&t); err != nil {
+		t.Kill(err)
+	}
+
+	// Wait for all threads to drop dead.
+	err := t.Wait()
+
+	// In case a special exit code is set, exit.
+	if err != nil {
+		os.Exit(1)
+	}
+
+	return nil
+}
+
+func runHTTP(logger *zap.Logger, handler http.Handler) func(t *tomb.Tomb) error {
+	return func(t *tomb.Tomb) error {
+		host := os.Getenv(hostEnvKey)
+		if host == "" {
+			host = "0.0.0.0"
+		}
+
+		port := os.Getenv(portEnvKey)
+		if port == "" {
+			port = defaultPort
+		}
+
+		logger = logger.With(zap.String("http_port", port))
+
+		// Start the server.
+		s := http.Server{
+			Addr:    fmt.Sprintf("%v:%s", host, port),
+			Handler: handler,
+		}
+
+		t.Go(func() error {
+			logger.Info("Starting the HTTP server...")
+
+			err := s.ListenAndServe()
+			if err == http.ErrServerClosed {
+				err = nil
+			}
+			if err != nil {
+				logger.Error("HTTP server crashed.", zap.Error(err))
+				return err
+			}
+
+			logger.Info("HTTP server terminated.")
+			return nil
+		})
+
+		// Shutdown on tomb dying.
+		t.Go(func() error {
+			<-t.Dying()
+
+			ctx, cancel := context.WithTimeout(context.Background(), HTTPServerShutdownTimeout)
+			defer cancel()
+
+			logger.Info("HTTP server shutdown in progress...", zap.Duration("timeout", HTTPServerShutdownTimeout))
+			return s.Shutdown(ctx)
+		})
+
+		return nil
+	}
+}
+
+func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.New()
+	if err := Run(logger, runHTTP(logger, server)); err != nil {
+		logger.Fatal("Failed to run server.", zap.Error(err))
+	}
+}
---
+++ src/go-api/go.mod
@@ -0,0 +1,9 @@
+module github.com/MarekVigas/Postar-Jano
+
+go 1.14
+
+require (
+	github.com/labstack/echo/v4 v4.1.16
+	go.uber.org/zap v1.15.0
+	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637
+)
