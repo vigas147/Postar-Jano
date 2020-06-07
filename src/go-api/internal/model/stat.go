ca10e5eb6b01639bedf416126ea6da83a7031484
Lukas Macko <xlukas.macko@gmail.com>
2020-06-07 10:54:20 +0200

    Stat limit can be null
    Signed-off-by: Lukas Macko <xlukas.macko@gmail.com>

--- src/go-api/internal/model/stat.go
+++ src/go-api/internal/model/stat.go
@@ -1,11 +1,11 @@
 package model
 
 type Stat struct {
-	EventID    int `json:"event_id" db:"event_id"`
-	DayID      int `json:"day_id" db:"day_id"`
-	Capacity   int `json:"capacity" db:"capacity"`
-	LimitBoys  int `json:"limit_boys" db:"limit_boys"`
-	LimitGirls int `json:"limit_girls" db:"limit_girls"`
-	BoysCount  int `json:"boys_count" db:"boys_count"`
-	GirlsCount int `json:"girls_count" db:"girls_count"`
+	EventID    int  `json:"event_id" db:"event_id"`
+	DayID      int  `json:"day_id" db:"day_id"`
+	Capacity   int  `json:"capacity" db:"capacity"`
+	LimitBoys  *int `json:"limit_boys" db:"limit_boys"`
+	LimitGirls *int `json:"limit_girls" db:"limit_girls"`
+	BoysCount  int  `json:"boys_count" db:"boys_count"`
+	GirlsCount int  `json:"girls_count" db:"girls_count"`
 }
